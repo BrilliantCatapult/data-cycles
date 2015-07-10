e58a7744b53188479c317d918d241f8734398a4e
Bahia El-Sharkawy <bahia.sharkawy@gmail.com>
2015-07-10 11:00:20 -0700

    (feat) setup server with elastic search package

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = require('./server/server.js');
+
+var port = process.env.PORT || 3000;
+
+
+app.listen(port, function () {
+  console.log("Listening to port " + port);
+});
