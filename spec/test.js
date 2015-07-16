8b8527ff89e347465ab5dae2e3292ac418a569fa
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-07-16 15:25:32 -0700

    (fix) deletes fake failing test

--- spec/test.js
+++ spec/test.js
@@ -2,7 +2,4 @@ describe("A suite", function() {
   it("should pass a test", function() {
     expect(true).toBe(true);
   });
-  it("should fail a test", function() {
-    expect(false).toBe(true);
-  });
 });
