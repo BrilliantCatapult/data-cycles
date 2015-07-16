e5f525884ce445bf96489aea6988a4c993369afd
Shanan Sussman <gfxbox@gmail.com>
2015-07-15 19:06:50 -0700

    refactored security for modular username and password

--- server/config/elasticsearch.js
+++ server/config/elasticsearch.js
@@ -1,7 +1,8 @@
 var elasticsearch = require('elasticsearch');
+var auth = require('./elasticAuth.js')
 
 var client = new elasticsearch.Client({
-    host: 'http://searchonly:ooc3id6jupw6e9u0ha@b856e0e6cb149c5f2876528086313183.us-east-1.aws.found.io:9200',
+    host: 'http://' + auth.username + ':' + auth.password + '@b856e0e6cb149c5f2876528086313183.us-east-1.aws.found.io:9200',
     //host: 'localhost:9200',
     log: 'trace'
   });
