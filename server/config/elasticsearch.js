0b1afbc2d8ccc61aa6fdab04df6f6843a38a4984
Shanan Sussman <gfxbox@gmail.com>
2015-07-15 18:58:17 -0700

    implemented basic db security

--- server/config/elasticsearch.js
+++ server/config/elasticsearch.js
@@ -1,7 +1,7 @@
 var elasticsearch = require('elasticsearch');
 
 var client = new elasticsearch.Client({
-    host: 'http://b856e0e6cb149c5f2876528086313183.us-east-1.aws.found.io:9200',
+    host: 'http://searchonly:ooc3id6jupw6e9u0ha@b856e0e6cb149c5f2876528086313183.us-east-1.aws.found.io:9200',
     //host: 'localhost:9200',
     log: 'trace'
   });
